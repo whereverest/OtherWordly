8e53a70a4963da99bb5bd29da7d22369ed60ee9f
Crazy 0xMonkey <taicomplexli@gmail.com>
2022-05-25 11:28:12 -0600

    4 frames longer animation

--- src/apps/firstPlay/pages/Welcome.svelte
+++ src/apps/firstPlay/pages/Welcome.svelte
@@ -1,52 +1,181 @@
-<div class={ `welcome-page
-              ${_IS_DEV_ENV && 'welcome-page--with-bg-image'}` }>
+<script>
+  const _IS_DEV_ENV = IS_DEV_ENV,
+    _IS_PROD_ENV = IS_PROD_ENV;
 
+  import { push } from "svelte-spa-router";
+  import { fade } from "svelte/transition";
+  import { onMount } from "svelte";
 
+  // Components
+  import BasicPageLayout from "@components/BasicPageLayout.svelte";
+  import ButtonInCircle from "@components/ButtonInCircle.svelte";
+  import Icon from "svelte-icon";
+
+  // Stores
+  // import { basicFontSize } from '@stores/miscellaneous.js';
+  import { transitionFrom, transitionTo } from "@stores-fp/transitions.js";
+
+  // Images
+  import randomAvatar from "@images/characters/char15_astra.png";
+
+  // Icons
+  import chatBubbleIcon from "@icons/chat-bubble.svg";
+  import { isPortraitMode } from "@stores/miscellaneous.js";
+
+  onMount(() => {
+    transitionFrom.update(() => "");
+    transitionTo.update(() => "");
+  });
+
+  // Show elements on page opening ----------------------------------------------
+  let isAllPageElementsVisible = false;
+  let visibleAbove = false;
+  //let visibleBelow = false;
+  onMount(() => {
+    setTimeout(() => (isAllPageElementsVisible = true), 300);
+    setTimeout(() => {
+      visibleAbove = true;
+    }, 100);
+    // setTimeout(() => {
+    //   visibleBelow = true;
+    // }, 600);
+  });
+
+  function fadeInEffectTextAbove(node) {
+    const valid =
+      node.childNodes.length === 1 &&
+      node.childNodes[0].nodeType === Node.TEXT_NODE;
+
+    if (!valid) {
+      throw new Error(
+        `This transition only works on elements with a single text node child`
+      );
+    }
+
+    const text = node.textContent;
+    const duration = text.length / (5 * 0.01);
+
+    return {
+      duration,
+      tick: (t) => {
+        const i = Math.trunc(text.length * t);
+        node.textContent = text.slice(0, i);
+      },
+    };
+  }
+
+  function fadeInEffectTextBelow(node) {
+    const valid =
+      node.childNodes.length === 1 &&
+      node.childNodes[0].nodeType === Node.TEXT_NODE;
+
+    if (!valid) {
+      throw new Error(
+        `This transition only works on elements with a single text node child`
+      );
+    }
+
+    const text = node.textContent;
+    const duration = text.length / (5 * 0.01);
+
+    return {
+      duration,
+      tick: (t) => {
+        const i = Math.trunc(text.length * t);
+        node.textContent = text.slice(0, i);
+      },
+    };
+  }
+
+  // Go to Challenges page ------------------------------------------------------
+  function goToChallengesPage() {
+    setTimeout(() => transitionFrom.update(() => "Welcome"), 300);
+    setTimeout(() => transitionTo.update(() => "Challenges"), 300);
+    setTimeout(() => push("/challenges"), 600);
+  }
+</script>
+
+<div
+  class={`welcome-page
+              ${_IS_DEV_ENV && "welcome-page--with-bg-image"}`}
+>
   <BasicPageLayout class="welcome-page__basic-layout-container">
-
     <!-- slot: content -->
     <svelte:fragment slot="content">
-      <div class={ `welcome-page__page-content
-                    ${($transitionTo === 'Challenges' || !isAllPageElementsVisible)
-                                            && 'welcome-page__page-content--invisible'}
-                    ${$transitionTo === 'Challenges' && 'welcome-page__page-content--moved-to-left'}` }>
-
+      <div
+        class={`welcome-page__page-content
+                    ${
+                      ($transitionTo === "Challenges" ||
+                        !isAllPageElementsVisible) &&
+                      "welcome-page__page-content--invisible"
+                    }
+                    ${
+                      $transitionTo === "Challenges" &&
+                      "welcome-page__page-content--moved-to-left"
+                    }`}
+      >
         <!-- Greeting snippet -->
-        <div class={`greeting-snippet
+        <div
+          class={`greeting-snippet
                     welcome-page__greeting-snippet 
-                    ${$isPortraitMode && 'greeting-snippet__portrait-mode'}`}>
-
-          <div class={`greeting-snippet__avatar-container ${$isPortraitMode && 'greeting-snippet__avatar-container__portrait-mode'}`}>
+                    ${$isPortraitMode && "greeting-snippet__portrait-mode"}`}
+        >
+          <div
+            class={`greeting-snippet__avatar-container ${
+              $isPortraitMode &&
+              "greeting-snippet__avatar-container__portrait-mode"
+            }`}
+          >
             <img
               class="greeting-snippet__avatar"
-              src={ randomAvatar }
+              src={randomAvatar}
               alt="avatar"
             />
           </div>
 
           <div class="greeting-snippet__text-n-button">
-
             <!-- Greeting text -->
-            <div class="greeting-text
-                        greeting-snippet__text">
-
+            <div
+              class="greeting-text
+                        greeting-snippet__text"
+            >
               <Icon
-                class={`greeting-text__chat-bubble-bg ${$isPortraitMode && 'chat-bubble-portrait-mode'}`}
-                data={ chatBubbleIcon }
+                class={`greeting-text__chat-bubble-bg ${
+                  $isPortraitMode && "chat-bubble-portrait-mode"
+                }`}
+                data={chatBubbleIcon}
               />
 
               <div class="greeting-text__title">
                 <em>Welcome</em>&nbsp; to Alphazoid Prime!
               </div>
-
-              <div class={`greeting-text__paragraph${$isPortraitMode && '__portrait-mode'}`}>
-                Join us on a journey by<br>
-                matching words.
+              <div
+                class={`greeting-text__paragraph${
+                  $isPortraitMode && "__portrait-mode"
+                }`}
+              >
+                {#if visibleAbove}
+                  <div class="fade-text-div-above" transition:fade={{ duration: 500 }}>
+                    <p transition:fadeInEffectTextAbove>
+                      Join us on a journey by matching the words.
+                    </p>
+                  </div>
+                {/if}
+                <!-- {#if visibleBelow}
+                  <div class="fade-text-div-below" transition:fade={{ duration: 500 }}>
+                    <p transition:fadeInEffectTextBelow>matching words</p>
+                  </div>
+                {/if} -->
               </div>
+            </div>
+            <!-- / Greeting text -->
 
-            </div><!-- / Greeting text -->
-
-            <div class={`greeting-snippet__button-container ${$isPortraitMode && 'greeting-snippet__button-container__portrait-mode'}`}>
+            <div
+              class={`greeting-snippet__button-container ${
+                $isPortraitMode &&
+                "greeting-snippet__button-container__portrait-mode"
+              }`}
+            >
               <div
                 class="tap-to-continue-but
                         character-speech__tap-to-continue-but"
@@ -54,155 +183,118 @@
                 on:click={goToChallengesPage}
               >
                 Tap to Continue
-              </div><!-- / Tap to continue but -->
+              </div>
+              <!-- / Tap to continue but -->
             </div>
-
           </div>
-
-        </div><!-- / Greeting snippet -->
-
+        </div>
+        <!-- / Greeting snippet -->
       </div>
     </svelte:fragment><!-- / slot: content -->
-
   </BasicPageLayout>
-
 </div>
 
-
-
-<script>
-const _IS_DEV_ENV = IS_DEV_ENV,
-      _IS_PROD_ENV = IS_PROD_ENV;
-
-import { push } from 'svelte-spa-router';
-import { fade } from 'svelte/transition';
-import { onMount } from 'svelte';
-
-// Components
-import BasicPageLayout from '@components/BasicPageLayout.svelte';
-import ButtonInCircle from '@components/ButtonInCircle.svelte';
-import Icon from 'svelte-icon';
-
-// Stores
-// import { basicFontSize } from '@stores/miscellaneous.js';
-import { transitionFrom, transitionTo } from '@stores-fp/transitions.js';
-
-// Images
-import randomAvatar from '@images/characters/char15_astra.png';
-
-// Icons
-import chatBubbleIcon from '@icons/chat-bubble.svg';
-import {isPortraitMode} from '@stores/miscellaneous.js';
-
-
-
-onMount(() => {
-  transitionFrom.update(() => '');
-  transitionTo.update(() => '');
-});
-
-
-// Show elements on page opening ----------------------------------------------
-let isAllPageElementsVisible = false;
-onMount(() => {
-  setTimeout(() => isAllPageElementsVisible = true, 300);
-});
-
-// Go to Challenges page ------------------------------------------------------
-function goToChallengesPage() {
-
-  setTimeout(() => transitionFrom.update(() => 'Welcome'), 300)
-  setTimeout(() => transitionTo.update(() => 'Challenges'), 300)
-  setTimeout(() => push('/challenges'), 600);
-}
-
-</script>
-
-
-
 <style lang="scss">
-
-
-// Greeting text --------------------------------------------------------------
-.greeting-text {
-  position: relative;
-  font-size: 2.05rem;
-  line-height: 2.7rem;
-
-  :global(.greeting-text__chat-bubble-bg) {
-    width: 17.0rem;
-    height: 9.5rem;
-    position: absolute;
-    inset: auto auto -2.2rem -5.5rem;
-    fill: transparent;
-    stroke: rgba(255,255,255,.2);
-    stroke-width: .5rem;
-    mask-image: linear-gradient(24deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 43%);
+  // Greeting text --------------------------------------------------------------
+  .fade-text-div-above {
+    margin-bottom: -1rem;
   }
-
-  :global(.chat-bubble-portrait-mode) {
-    left: -2rem;
+  .fade-text-div-below {
+    margin-bottom: -1rem;
   }
-
-  &__title {
-    margin-bottom: 1.0rem;
+  .greeting-text {
+    position: relative;
     text-align: left;
-    white-space: nowrap;
+    font-size: 1.56rem;
+    line-height: 2.7rem;
 
-    em {
-      font-size: 2.9rem;
-      font-family: 'ZingScriptRustR-Base',serif;
-      font-style: normal;
-      font-feature-settings: 'swsh';
-      color: #FBFEA6;
+    :global(.greeting-text__chat-bubble-bg) {
+      width: 17rem;
+      height: 9.5rem;
+      position: absolute;
+      inset: auto auto -2.2rem -5.5rem;
+      fill: transparent;
+      stroke: rgba(255, 255, 255, 0.2);
+      stroke-width: 0.5rem;
+      mask-image: linear-gradient(
+        24deg,
+        rgba(0, 0, 0, 1) 0%,
+        rgba(0, 0, 0, 0) 43%
+      );
+    }
+
+    :global(.chat-bubble-portrait-mode) {
+      left: -2rem;
+    }
+
+    &__title {
+      margin-bottom: 1rem;
+      text-align: left;
+      white-space: nowrap;
+
+      em {
+        font-size: 2.9rem;
+        font-family: "ZingScriptRustR-Base", serif;
+        font-style: normal;
+        font-feature-settings: "swsh";
+        color: #fbfea6;
+      }
+    }
+
+    &__paragraph {
+      text-align: left;
+
+      animation-duration: 4s;
+      animation-name: scale-bounce;
+      animation-iteration-count: infinite;
+
+      @keyframes scale-bounce {
+        from {
+          transform: scale(1);
+        }
+        7% {
+          transform: scale(1.1);
+        }
+        25% {
+          transform: scale(0.95);
+        }
+        50% {
+          transform: scale(1);
+        }
+        to {
+          transform: scale(1);
+        }
+      }
+
+      &__portrait-mode {
+        text-align: center;
+      }
     }
   }
 
-  &__paragraph {
-    text-align: left;
-
-    animation-duration: 4s;
-    animation-name: scale-bounce;
-    animation-iteration-count: infinite;
-
-    @keyframes scale-bounce {
-      from {transform: scale(1);}
-      7% {transform: scale(1.1);}
-      25% {transform: scale(.95);}
-      50% {transform: scale(1);}
-      to {transform: scale(1);}
-    }
-
-    &__portrait-mode {
-      text-align: center;
-    }
-  }
-}
-
-
-// Greeting snippet -----------------------------------------------------------
-.greeting-snippet {
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-
-  &__portrait-mode {
-    display: block;
-    right: 0 !important;
-  }
-
-  &__avatar-container {
-    width: 13.0rem;
-    height: 13.0rem;
-    margin: 4.0rem 8.6rem 0 0;
+  // Greeting snippet -----------------------------------------------------------
+  .greeting-snippet {
+    box-sizing: border-box;
     display: flex;
     justify-content: center;
-    align-items: center;
 
     &__portrait-mode {
-      margin: auto;
+      display: block;
+      right: 0 !important;
     }
-  }
+
+    &__avatar-container {
+      width: 13rem;
+      height: 13rem;
+      margin: 4rem 8.6rem 0 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &__portrait-mode {
+        margin: auto;
+      }
+    }
 
     &__avatar {
       width: 100%;
@@ -210,14 +302,14 @@ function goToChallengesPage() {
       object-fit: contain;
     }
 
-  &__text-n-button {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
+    &__text-n-button {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    }
 
     &__text {
-      margin-bottom: 4.0rem;
+      margin-bottom: 4rem;
     }
 
     &__button-container {
@@ -237,45 +329,53 @@ function goToChallengesPage() {
       }
 
       @keyframes scale-bounce {
-        from {transform: scale(1);}
-        7% {transform: scale(1.1);}
-        25% {transform: scale(.95);}
-        50% {transform: scale(1);}
-        to {transform: scale(1);}
+        from {
+          transform: scale(1);
+        }
+        7% {
+          transform: scale(1.1);
+        }
+        25% {
+          transform: scale(0.95);
+        }
+        50% {
+          transform: scale(1);
+        }
+        to {
+          transform: scale(1);
+        }
       }
     }
 
-      :global(.greeting-snippet__button) {
-        transform: scale(2.3);
-        transform-origin: center center;
+    :global(.greeting-snippet__button) {
+      transform: scale(2.3);
+      transform-origin: center center;
+    }
+  }
+
+  // Welcome page ---------------------------------------------------------------
+  .welcome-page {
+    &--with-bg-image {
+      background: 50% 50% / cover url(../../../assets/images/menu_bg2.jpg)
+        no-repeat;
+    }
+
+    &__page-content {
+      opacity: 1;
+      transition: margin 0.6s ease, opacity 0.4s ease;
+
+      &--invisible {
+        opacity: 0;
       }
-}
 
-
-// Welcome page ---------------------------------------------------------------
-.welcome-page {
-
-  &--with-bg-image {
-    background: 50% 50%/cover url(../../../assets/images/menu_bg2.jpg) no-repeat;
-  }
-
-  &__page-content {
-    opacity: 1;
-    transition: margin .6s ease, opacity .4s ease;
-
-    &--invisible {
-      opacity: 0;
+      &--moved-to-left {
+        margin-left: -25rem;
+      }
     }
-
-    &--moved-to-left {
-      margin-left: -25.0rem;
-    }
-  }
 
     &__greeting-snippet {
       position: relative;
       inset: auto 3.9rem auto auto;
     }
-}
-
+  }
 </style>
